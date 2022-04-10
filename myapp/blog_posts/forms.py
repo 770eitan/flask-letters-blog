e914f0230c06f01676cfc86e1ddf17990442ea3a
Eitan Shmuel Corenblum <carl158@gmail.com>
2022-04-10 13:48:51 -0400

    finish functionality

--- myapp/blog_posts/forms.py
+++ myapp/blog_posts/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
 from wtforms.validators import DataRequired
 
 class BlogPostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     text = TextAreaField('Text', validators=[DataRequired()])
     submit = SubmitField('Post')
+    profile = FileField('Profile', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
